666c8e33f40e21f906a58a6c47a6205111e9423f
TakayaEnomoto <kumagplus99@gmail.com>
2019-11-25 05:51:10 +0800

    Changed some script details. Added a thrid enemy, which is still not working yet. Something went wrong in the script.

---
+++ TeamSuperMarioGalaxy/Assets/AlexFolder/Scripts/PiranhaPlant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiranhaPlant : MonoBehaviour
+{
+    public Transform Player;//Assign player to this transform so that the plant can look at the player.
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z), Vector3.left);
+    }
+}
---
+++ TeamSuperMarioGalaxy/Assets/AlexFolder/Scripts/TestPlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerScript : MonoBehaviour
+{
+    public Vector3 inputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        inputVector = transform.forward * vertical;
+        inputVector += transform.right * horizontal;
+        inputVector.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(inputVector.x, GetComponent<Rigidbody>().velocity.y, inputVector.z);
+    }
+}
